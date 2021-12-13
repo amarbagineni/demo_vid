4a5b2a635724e3f2fb96d2980004a3e29faa0bd8
shubhamsearce <shubham.sahu@searce.com>
2021-12-13 19:18:54 +0530

    Upload video completed

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,36 @@
-console.log("This is some message yyyy")
+console.log("This is some message yyyy");
 
 function showMessage(id, message) {
-    var errorMsg = document.getElementById(id);
-    errorMsg.innerHTML = message;
-    errorMsg.style.display = "block";
+  var errorMsg = document.getElementById(id);
+  errorMsg.innerHTML = message;
+  errorMsg.style.display = "block";
 }
 
 function hideMessage(id, message) {
-    var errorMsg = document.getElementById(id);
-    errorMsg.innerHTML = message;
-    errorMsg.style.display = "none";
+  var errorMsg = document.getElementById(id);
+  errorMsg.innerHTML = message;
+  errorMsg.style.display = "none";
 }
 
-function startupload() {
-    console.log("Start the video uploda here");
-    showMessage("upload-error", "Someerror from func")
+async function startupload() {
+  console.log("Start the video uploda here");
+  let video = document.getElementById("videoSourceFile").files[0];
+  let videoTitle = document.getElementById("videoTitle").value;
+
+  let formData = new FormData();
+
+  formData.append("videoFile", video);
+  formData.append("title", videoTitle);
+
+  try {
+    await fetch('http://35.200.187.58:3000/api/' + "videohotspot/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    showMessage("upload-error", "File uploaded successfully!");
+  } catch (err) {
+    console.log(err);
+    showMessage("upload-error", "Something went wrong!");
+  }
 }
